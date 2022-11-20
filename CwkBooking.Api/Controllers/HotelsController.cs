db45c9cd76301747db996034115fe966e7a6cea6
Nicholas Patterson <nicholaspatterson@nicholass-air.myfiosgateway.com>
2022-11-20 17:07:23 -0500

    basic crud endpoints for hotels

--- CwkBooking.Api/Controllers/HotelsController.cs
+++ CwkBooking.Api/Controllers/HotelsController.cs
@@ -1,22 +1,79 @@
 ﻿using System;
+using CwkBooking.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CwkBooking.Api.Controllers
 {
 
     [ApiController]
-    [Route("[controller]")]
-    public class HotelsController : Controller
+    [Route("api/[controller]")]
+    public class HotelsController : ControllerBase
     {
-        public HotelsController()
-        {
+        private readonly DataSource _dataSource;
 
+        public HotelsController(DataSource dataSource)
+        {
+            _dataSource = dataSource;
         }
 
         [HttpGet]
-        public ActionResult<string> GetRooms()
+        public ActionResult<List<Hotel>> GetAllHotels()
         {
-            return "Hello from hotels controller";
+            var hotels = _dataSource.Hotels;
+            return hotels;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Hotel> GetHotelById(int id)
+        {
+            var hotels = _dataSource.Hotels;
+            var hotel = hotels.FirstOrDefault(hotel => hotel.HotelId == id);
+
+            if(hotel == null)
+            {
+                return NotFound();
+            }
+            return hotel;
+        }
+
+        [HttpPost]
+        public ActionResult<Hotel> CreateHotel([FromBody] Hotel hotel)
+        {
+            var hotels = _dataSource.Hotels;
+            hotels.Add(hotel);
+
+            return CreatedAtAction(nameof(GetHotelById), new { id = hotel.HotelId }, hotel);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateHotel(int id, [FromBody] Hotel hotel)
+        {
+            var hotels = _dataSource.Hotels;
+            var hotelToBeUpdated = hotels.FirstOrDefault(hotel => hotel.HotelId == id);
+
+            if(hotelToBeUpdated == null)
+            {
+                return NotFound();
+            }
+
+            hotels.Remove(hotelToBeUpdated);
+            hotels.Add(hotel);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteHotel(int id)
+        {
+            var hotels = _dataSource.Hotels;
+            var hotelToDelete = hotels.FirstOrDefault(hotel => hotel.HotelId == id);
+
+            if (hotelToDelete == null)
+            {
+                return NotFound();
+            }
+
+            hotels.Remove(hotelToDelete);
+            return NoContent();
         }
     }
 }
---
+++ CwkBooking.Api/DataSource.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using CwkBooking.Domain.Models;
+
+namespace CwkBooking.Api
+{
+    public class DataSource
+    {
+        public DataSource()
+        {
+            Hotels = GetHotels();
+        }
+
+        public List<Hotel> Hotels { get; set; }
+
+        private static List<Hotel> GetHotels()
+        {
+            return new List<Hotel>
+            {
+                new Hotel
+                {
+                    HotelId = 1,
+                    Name = "Conquistador",
+                    Stars = 3,
+                    Address = "",
+                    Country = "Puerto Rico",
+                    City = "Fajardo",
+                    Description = "Some nice description"
+                },
+
+                new Hotel
+                {
+                    HotelId = 2,
+                    Name = "The Westin",
+                    Stars = 4,
+                    Address = "",
+                    Country = "USA",
+                    City = "Seattle",
+                    Description = "Some nice description"
+                }
+            };
+        }
+    }
+}
+
